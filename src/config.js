d7e24d560d46875a80b31e4025c2ab14048bf45c
Gregor Anders <gregor.anders@gmail.com>
2015-04-13 18:04:08 +0200

    #1 - added missing file

---
+++ src/config.js
@@ -0,0 +1,37 @@
+System.config({
+  "paths": {
+    "*": "*.js",
+    "github:*": "vendor/github/*.js"
+  }
+});
+
+System.config({
+  "map": {
+    "angular": "github:angular/bower-angular@master",
+    "angular-animate": "github:angular/bower-angular-animate@master",
+    "angular-aria": "github:angular/bower-angular-aria@master",
+    "angular-cookies": "github:angular/bower-angular-cookies@master",
+    "angular-i18n": "github:angular/bower-angular-i18n@master",
+    "angular-material": "github:angular/bower-material@master",
+    "angular-messages": "github:angular/bower-angular-messages@master",
+    "angular-mocks": "github:angular/bower-angular-mocks@master",
+    "angular-resource": "github:angular/bower-angular-resource@master",
+    "angular-route": "github:angular/bower-angular-route@master",
+    "angular-sanitize": "github:angular/bower-angular-sanitize@master",
+    "angular-touch": "github:angular/bower-angular-touch@master",
+    "angular-translate": "github:angular-translate/bower-angular-translate@master",
+    "angular-translate-loader-static-files": "github:angular-translate/bower-angular-translate-loader-static-files@master",
+    "moment": "github:moment/moment@master",
+    "traceur": "github:jmcriffey/bower-traceur@0.0.87",
+    "traceur-runtime": "github:jmcriffey/bower-traceur-runtime@0.0.87",
+    "github:angular/bower-angular-animate@1.3.15": {
+      "angular": "github:angular/bower-angular@master"
+    },
+    "github:angular/bower-material@master": {
+      "angular": "github:angular/bower-angular@master",
+      "angular-animate": "github:angular/bower-angular-animate@master",
+      "hammer": "github:hammerjs/hammer.js@2.0.4"
+    }
+  }
+});
+
